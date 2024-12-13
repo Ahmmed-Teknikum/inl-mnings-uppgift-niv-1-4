862ca9626df89429c99b9bb50ad67621d0500fe6
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-13 09:03:47 +0100

    Add project files.

---
+++ inlämnings uppgift nivå 1 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inlämningsUppgift_._nivå1_._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+
+            do
+            {
+                Console.WriteLine("Ange lösenordet");
+                password = Console.ReadLine();
+            }
+            while (password != "hemligt");
+            Console.WriteLine("Rätt lösenord");
+
+        }
+    }
+}
